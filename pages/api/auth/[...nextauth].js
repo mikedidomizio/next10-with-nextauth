47ed9cb3430ef4180af64419557b61fd50158abe
Mike DiDomizio <mikedidomizio@gmail.com>
2022-04-15 15:57:02 -0400

    Output env vars

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,6 +1,10 @@
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
 
+console.log(process.env.GITHUB_ID);
+console.log(process.env.GITHUB_SECRET.substring(0, 5));
+console.log(process.env.NEXTAUTH_URL);
+
 export default NextAuth({
     // Configure one or more authentication providers
     providers: [
